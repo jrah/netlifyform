15dcedfd056010874521df97d3ccb5203d5446d4
jrah <jjames.home@gmail.com>
2019-01-16 03:42:31 +0000

    Add netlify form for testing

---
+++ pages/index.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="content">
+    <appForm/>
+  </div>
+</template>
+
+<script>
+import home from "~/content/home.json";
+
+import appForm from "~/components/Form.vue";
+export default {
+  components: {
+    appForm
+  }
+};
+</script>
+
+<style scoped lang="scss">
+.pin-container {
+  position: absolute;
+  &-r {
+    right: 4rem;
+  }
+}
+</style>
